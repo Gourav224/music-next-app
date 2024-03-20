46f8684a65c307dae20d4dabbd498564be8c82c5
gkrdhiman <gkrdhiman@gmail.com>
2024-03-20 07:49:17 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Gourav Kumar Dhiman</h1>
+    </main>
+  );
+}
