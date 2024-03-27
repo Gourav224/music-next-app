6fcfc384769916d101733c2590540b37b69d2cf2
gkrdhiman <gkrdhiman@gmail.com>
2024-03-27 08:43:34 +0000

    add infinte moving card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
-      <FeaturedCourses/>
-      <WhyChooseUs/>
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
     </main>
   );
 }
