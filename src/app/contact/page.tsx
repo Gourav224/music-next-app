e36f78ebf07773fb445d5a83033e8fcd5be2117d
Gourav224 <gkrdhiman@gmail.com>
2024-06-15 12:35:29 +0530

    tailwind error fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,9 +2,11 @@
 
 import React, { FormEvent, useState } from "react";
 import { BackgroundBeams } from "@/components/ui/meteors";
-const page = () => {
+
+const Page = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Submitted:", { email, message });
@@ -12,13 +14,11 @@ const page = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
-      {" "}
       {/* Ensure the container is relative */}
       {/* BackgroundBeams with adjusted z-index */}
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       {/* Content with higher z-index */}
       <div className="max-w-2xl mx-auto p-4 relative z-10">
-        {" "}
         {/* Add relative and z-10 to bring content to the front */}
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
           Contact Us
@@ -36,6 +36,7 @@ const page = () => {
             placeholder="Your email address"
             className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
             required
+            aria-label="Email address"
           />
           <textarea
             value={message}
@@ -44,6 +45,7 @@ const page = () => {
             className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
             rows={5}
             required
+            aria-label="Message"
           ></textarea>
           <button
             type="submit"
@@ -57,4 +59,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,10 @@ import type { Config } from "tailwindcss";
 import svgToDataUri from "mini-svg-data-uri";
 import defaultTheme from "tailwindcss/defaultTheme";
 import colors from "tailwindcss/colors";
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
